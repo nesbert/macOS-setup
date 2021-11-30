4e000231ce7fc1c8bf2c20eb14d2d399c4263a7d
Nesbert Hidalgo <nesbert@gmail.com>
2021-11-29 20:53:58 -0800

    Closes #4 adding nvm support to `.zprofile`

--- install.sh
+++ install.sh
@@ -138,6 +138,21 @@ echo "Installing Zulu OpenJDKs..."
 brew tap mdogan/zulu
 brew install --cask ${JDKs[@]}
 
+# Initialize Node Version Manager
+if ! grep -q "nvm" ${HOME_ZSHRC}; then
+  # create nvm dir
+  [ -d "$HOME/.nvm" ] || mkdir "$HOME/.nvm"
+  echo "Added nvm to ${HOME_ZSHRC}."
+  echo '' >> ${HOME_ZSHRC}
+  echo '# nvm support' >> ${HOME_ZSHRC}
+  # Set nvm workspace
+  echo 'NVM_DIR="$HOME/.nvm"' >> ${HOME_ZSHRC}
+  # Load nvm
+  echo '[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh"' >> ${HOME_ZSHRC}
+  # Load nvm bash_completion
+  echo '[ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"' >> ${HOME_ZSHRC}
+fi
+
 # List Cask Apps
 CASKS=(
   # aldente
